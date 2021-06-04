d5eef6152cd76280dbfc793f3060dd5e09dc7d5b
parinitha25 <parinithacn@gmail.com>
2021-06-04 19:39:11 +0530

    first commit

--- public/src/App.js
+++ public/src/App.js
@@ -1,10 +1,9 @@
 import React  from 'react';
-import RouterComponent from './components/Router-component/RouterComponent';
 
 export default function App() {
   return (
     <div>
-      <RouterComponent/>
+      <h1>react application</h1>
     </div>
   );
 }
